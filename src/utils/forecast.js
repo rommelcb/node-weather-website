0391e758e4c812bad5a5d0ee499131306659ee0a
Blas <rommel.blas@gmail.com>
2022-02-23 14:23:52 +0800

    Refactor the forecast code.

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -5,13 +5,16 @@ const forecast = (latitude, longitude, callback) => {
     // units: m=celsius, f=fahrenheit, s=kelvin,
 
     request({ url, json: true }, (error, { body }) => {
-        if (error) {
+        if (error)
             callback('Unable to connect to weather service!', undefined)
-        } else if (body.error) {
+        else if (body.error)
             callback('Unable to find location!', undefined)
-        } else {
-            callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees outside; but feels like ${body.current.feelslike} degrees. The humidity is ${body.current.humidity}%.`)
-        }
+        else
+            callback(
+                undefined, 
+                `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees outside; but feels like 
+                 ${body.current.feelslike} degrees. The humidity is ${body.current.humidity}%.`
+            )
     })
 }
 
